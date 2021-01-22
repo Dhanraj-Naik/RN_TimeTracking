c67b34f0c60555e2931aa3f76ceade66edb74419
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2021-01-22 21:50:19 +0530

    -timerapp

--- src/App.js
+++ src/App.js
@@ -14,18 +14,180 @@ import {
   View,
   Text,
   StatusBar,
+  KeyboardAvoidingView
 } from 'react-native';
+// import 'react-native-get-random-values';
+import { v4 as uuid } from 'uuid';
+import EditableTimer from './components/EditableTimer';
+import ToggleableTimerForm from './components/ToggleableTimerForm';
+import { newTimer } from './utils/TimerUtils';
 
-const App = () => {
-  return (
-    <View>
-      <Text>{'Hello world'}</Text>
-    </View>
+const TAG = 'App.js'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timers: [{
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        // id: uuid(),
+        id: 22,
+        elapsed: 5456099,
+        isRunning: true,
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        // id: uuid(),
+        id: 33,
+        elapsed: 1273998,
+        isRunning: false,
+      },],
+
+    }
+  }
+
+  componentDidMount = () => {
+    const TIME_INTERVAL = 1000;
+    this.intervalId = setInterval(() => {
+      const { timers } = this.state;
+
+      this.setState({
+        timers: timers.map(timer => {
+          const { elapsed, isRunning } = timer;
+
+          return {
+            ...timer,
+            elapsed: isRunning ? elapsed + TIME_INTERVAL : elapsed
+          }
+        })
+      })
+    }, TIME_INTERVAL)
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.intervalId);
+  }
+
+  handleCreateFormSubmit = timer => {
+    const { timers } = this.state;
+    this.setState({
+      timers: [newTimer(timer), ...timers],
+    });
+  };
+
+  handleFormSubmit = attrs => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: timers.map(timer => {
+        if (timer.id === attrs.id) {
+          const { title, project } = attrs;
+
+          return {
+            ...timer,
+            title,
+            project,
+          }
+        } else {
+          return timer
+        }
+      })
+    })
+  }
+
+  handleRemovePress = timerId => {
+    this.setState({
+      timers: this.state.timers.filter(t => t.id !== timerId)
+    })
+  }
+
+  handleCreateSubmit = timer => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: [newTimer(timer), ...timers]
+    })
+  }
+
+  toggleTimer = timerId => {
+    this.setState(prevState => {
+      const { timers } = this.state;
+
+      return {
+        timers: timers.map(timer => {
+          const { id, isRunning } = timer;
+
+          if (id === timerId) {
+            return {
+              ...timer,
+              isRunning: !isRunning,
+            };
+          }
+          return timer;
+        })
+      }
+    })
+  }
+
+  renderEditableTimers = () => (
+    this.state.timers.map(({ title, project, id, elapsed, isRunning }) => (
+      <EditableTimer
+        key={id}
+        id={id}
+        title={title}
+        project={project}
+        elapsed={elapsed}
+        isRunning={isRunning}
+        onFormSubmit={this.handleFormSubmit}
+        onRemovePress={this.handleRemovePress}
+        onStartPress={this.toggleTimer}
+        onStopPress={this.toggleTimer}
+      />
+    ))
   );
-};
+
+  render() {
+    const { timers } = this.state;
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <KeyboardAvoidingView behavior="padding" style={styles.timerListContainer}>
+          <ScrollView style={styles.timerList}>
+            <ToggleableTimerForm isOpen={false} onFormSubmit={this.handleCreateSubmit} />
+            {this.renderEditableTimers()}
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </View>
+    )
+  }
+}
+
 
 const styles = StyleSheet.create({
- 
+  appContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D7DA',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  timerList: {
+    paddingBottom: 15,
+  },
+  timerListContainer: {
+    flex: 1,
+  }
 });
 
 export default App;
